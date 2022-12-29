b0af05181dd71815e20a85c0a839af9625a5783b
IL8227 <iliaspb2012@gmail.com>
2022-12-30 01:22:02 +0300

    corection1

---
+++ Task1.java
@@ -0,0 +1,26 @@
+class Task1 {
+    public static void main(String[] args) {
+
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int n = grid.length;
+        if (n == 0)
+            return 0;
+        int i, j, m = grid[0].length, ans = 0;
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < m; j++) {
+                ans = Math.max(ans, solve(grid, i, j));
+            }
+        }
+        return ans;
+    }
+
+    public int solve(int[][] grid, int x, int y) {
+
+        if (x == -1 || y == -1 || x == grid.length || y == grid[0].length || grid[x][y] == 0)
+            return 0;
+        grid[x][y] = 0;
+        return solve(grid, x + 1, y) + solve(grid, x - 1, y) + solve(grid, x, y + 1) + solve(grid, x, y - 1) + 1;
+    }
+}
